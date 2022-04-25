6d1c2920cd8d111eb6ebb872d4b04aca2ad21970
arnaucv <arnaucv2@gmail.com>
2022-04-25 18:59:23 +0200

    Añadida condición de victoria - Arnaucv

---
+++ src/methods/gameMethods.php
@@ -0,0 +1,31 @@
+<?php
+
+$correctAnswers = ["Demon's Souls","Big Boss","Varré","Samus Aran","Arthas Menethil","Dracula",
+"Halo","Adolf Hitler","Todas son correctas","FFIV"];
+
+$victoryCondition = 5;
+
+function checkAnswer(string $answer, int $questionId){
+
+    global $correctAnswers;
+
+    if ($answer !=  $correctAnswers[$questionId-1]){
+        return false;
+    }
+    return true;
+}
+
+function victoryCheck(int $totalCorrect){
+
+    global $victoryCondition;
+
+    if ($totalCorrect != null && $totalCorrect < $victoryCondition) {
+        return false;
+    }
+    return true;
+}
+
+
+
+
+?>
