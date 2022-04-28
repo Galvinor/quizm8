b569082f148d97b728b7a56d57d0c0cfa026e51d
arnaucv <arnaucv2@gmail.com>
2022-04-28 15:29:46 +0200

    Añadido un método de carga, a adaptar por los del frontend la salida (actualmente envia h2 y inputs, modificadlo si quereis simplemente los datos para introducirlos en cajas) - Arnau

---
+++ src/methods/load.php
@@ -0,0 +1,20 @@
+<?php
+require('../controllers/conn.php');
+
+if($_POST) {
+    $form=$_REQUEST;
+    $searchValue = (int)$form['quizId']+1;
+    $consulta=$conexion->prepare("SELECT id, pregunta, respuesta1, respuesta2, respuesta3, respuesta4 FROM quiz WHERE id=".$searchValue."");
+    $consulta->execute();
+    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    
+    // enviamos el resultado
+    echo "
+    <h2>".$resultado['pregunta']."</h2>
+    <input type='number' style='display:none;' name='quizId' id='quizId' value='".$resultado['id']."'></input><br>
+    <input type='radio' name='encuesta' id='encuesta' value='1'>".$resultado['respuesta1']."<br>
+    <input type='radio' name='encuesta' id='encuesta' value='2'>".$resultado['respuesta2']."<br>
+    <input type='radio' name='encuesta' id='encuesta' value='3'>".$resultado['respuesta3']."<br>
+    <input type='radio' name='encuesta' id='encuesta' value='3'>".$resultado['respuesta4']."<br>
+    <input type='submit' value='Enviar' class='boton' id='btenviar'>";
+}
